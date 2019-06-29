1f3e0c722f47cc686643388fea4db9c13bc4399b
ay <750479899@qq.com>
2019-06-29 21:26:07 +0800

    java

---
+++ src/com/itheima/MyDemo.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class MyDemo {
+
+
+    public static void main(String[] args) {
+        System.out.println("zhoooooooooooooooooo");
+        System.out.println("zhoooooooooooooooooo");
+        System.out.println("zhoooooooooooooooooo");
+        System.out.println("zhoooooooooooooooooo");
+    }
+}
